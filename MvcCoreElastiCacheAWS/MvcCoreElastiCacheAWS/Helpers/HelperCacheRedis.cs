8d6190a050d1a5ec1fa18221de62ed069b125d3f
Alejandro López Labajo <mainusone1@gmail.com>
2024-05-21 13:25:16 +0200

    Commit

---
+++ MvcCoreElastiCacheAWS/MvcCoreElastiCacheAWS/Helpers/HelperCacheRedis.cs
@@ -0,0 +1,21 @@
+﻿using StackExchange.Redis;
+
+namespace MvcCoreElastiCacheAWS.Helpers
+{
+    public class HelperCacheRedis
+    {
+        private static Lazy<ConnectionMultiplexer> CreateConnection = new Lazy<ConnectionMultiplexer>(() =>
+        {
+            // AQUÍ IRA NUESTRA CADENA DE CONEXIÓN
+            return ConnectionMultiplexer.Connect("");
+        });
+
+        public ConnectionMultiplexer Connection
+        {
+            get
+            {
+                return CreateConnection.Value;
+            }
+        }
+    }
+}
