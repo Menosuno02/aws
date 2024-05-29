182f275202da9c410180538ad5201b4504e2dc8c
Alejandro López Labajo <mainusone1@gmail.com>
2024-05-29 12:57:22 +0200

    Commit

--- ApiPeliculasAWS/ApiPeliculasAWS/obj/Release/net8.0/linux-x64/ApiPeliculasAWS.AssemblyInfo.cs
+++ ApiPeliculasAWS/ApiPeliculasAWS/obj/Release/net8.0/linux-x64/ApiPeliculasAWS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiPeliculasAWS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8fcc032342f1d392545e487a639355736f3736d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+942d36234e97ed904cb07591895cd73183f6d95e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiPeliculasAWS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiPeliculasAWS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
